2885c920ef0247f87f640f0b686585d80a5d3efb
BARBE Nicolas <nicolas.barbe@econocom.com>
2020-07-24 12:15:29 +0200

    Manage null filter

---
+++ Linq2CouchBaseLiteExpression.Tests/AdvancedQueries/NullUnitTests.cs
@@ -0,0 +1,51 @@
+using Linq2CouchBaseLiteExpression.Tests.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Linq2CouchBaseLiteExpression.Tests.AdvancedQueries
+{
+    [TestClass]
+    public class NullUnitTests : BaseUnitTest
+    {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+        }
+
+        [TestCleanup]
+        public override void CloseConnection()
+        {
+            base.CloseConnection();
+        }
+
+        [TestMethod]
+        public void EqualsNull_Exists()
+        {
+            CheckCount<EntityObject>((e) => e.SurName == null, 1);
+        }
+
+        [TestMethod]
+        public void NullEquals_Exists()
+        {
+            CheckCount<EntityObject>((e) => null == e.SurName, 1);
+        }
+
+        [TestMethod]
+        public void IsNotNull_Exists()
+        {
+            CheckCount<EntityObject>((e) => e.SurName != null, 4);
+        }
+
+        [TestMethod]
+        public void NotNullIs_Exists()
+        {
+            CheckCount<EntityObject>((e) => null != e.SurName, 4);
+        }
+
+        [TestMethod]
+        public void NullableIsNull_Exists()
+        {
+            CheckCount<EntityObject>((e) => e.BirthDay == null, 2);
+        }
+    }
+}
